fa646a4a2f57841f211080c453ea9e10c720b43b
tomkochi <tomatkochi@gmail.com>
2022-03-22 21:20:11 +0530

    submit button bg reduced

--- components/loading.jsx
+++ components/loading.jsx
@@ -6,19 +6,31 @@ const Loading = ({ height }) => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="164"
-        height="164"
+        height={height}
         viewBox="0 0 164 164"
       >
         <g fill="none" fill-rule="evenodd">
-          <circle className={style.one} cx="36" cy="79" r="15" fill="#69AA72" />
           <circle
-            className={style.three}
-            cx="129"
-            cy="79"
+            className={style.one}
+            cx="36"
+            cy={height - 20}
             r="15"
             fill="#69AA72"
           />
-          <circle className={style.two} cx="82" cy="79" r="15" fill="#69AA72" />
+          <circle
+            className={style.three}
+            cx="129"
+            cy={height - 20}
+            r="15"
+            fill="#69AA72"
+          />
+          <circle
+            className={style.two}
+            cx="82"
+            cy={height - 20}
+            r="15"
+            fill="#69AA72"
+          />
         </g>
       </svg>
     </>
