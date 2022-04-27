28d833ed9ada64180f60b44c2f0835c16a111510
tomkochi <tomatkochi@gmail.com>
2022-04-27 21:37:32 +0530

    contact page form submission done

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,8 +1,79 @@
 import style from "./contact.module.scss";
 import Header from "../components/guestHeader";
 import Link from "next/link";
+import axios from "axios";
+import { useState } from "react";
+import Loading from "../components/loading";
 
 const Contact = ({ authKey }) => {
+	const [submitMessage, setSubmitMessage] = useState(null);
+	const [busy, setBusy] = useState(false);
+
+	const displayMessage = (t, m) => {
+		setSubmitMessage({
+			type: t,
+			message: m,
+		});
+		setTimeout(() => {
+			setSubmitMessage(null);
+		}, 3000);
+	};
+
+	const submit = (e) => {
+		setBusy(true);
+		e.preventDefault();
+		const fd = document.getElementById("fd").value;
+		if (fd.length > 0) {
+			displayMessage("error", "Suspectful activity detected.");
+			return;
+		}
+		const formData = {};
+		Object.entries(e.target).map((o) => {
+			if (o[1].name) {
+				formData[o[1].name] = o[1].value;
+			}
+		});
+
+		if (
+			!formData.name.trim() ||
+			!formData.email.trim() ||
+			!formData.organisation.trim() ||
+			!formData.title.trim() ||
+			!formData.message.trim()
+		) {
+			displayMessage("error", "All fields are required.");
+			return;
+		}
+
+		axios({
+			url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact/form`,
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+				authKey,
+			},
+			data: formData,
+		})
+			.then((r) => {
+				console.log(r.data.success);
+				if (r.data.success) {
+					displayMessage("success", "Your message has been sent");
+					Array.from(document.querySelectorAll("form")[0].elements).forEach(
+						(input) => {
+							input.value = "";
+						}
+					);
+				} else {
+					displayMessage("error", r.data.msg);
+				}
+			})
+			.catch((r) => {
+				displayMessage("error", "Something went wrong; try later.");
+			})
+			.finally(() => {
+				setBusy(false);
+			});
+	};
 	return (
 		<div className={style.contact}>
 			<Header authKey={authKey}>
@@ -15,11 +86,24 @@ const Contact = ({ authKey }) => {
 			<div className={style.contents}>
 				<div className={style.contentWrapper}>
 					<h1>Contact us</h1>
-					<form>
+					<form onSubmit={submit}>
 						<div className={style.row}>
+							<input
+								type="text"
+								id="fd"
+								className={style.fraud}
+								tabIndex="-1"
+							/>
 							<div className={style.inputGroup}>
 								<label htmlFor="name">Name</label>
-								<input type="text" name="name" id="name" placeholder="Name" />
+								<input
+									type="text"
+									name="name"
+									id="name"
+									placeholder="Name"
+									required
+									disabled={busy}
+								/>
 							</div>
 							{/* .inputGroup */}
 							<div className={style.inputGroup}>
@@ -29,6 +113,8 @@ const Contact = ({ authKey }) => {
 									name="title"
 									id="title"
 									placeholder="e.g. Manager"
+									required
+									disabled={busy}
 								/>
 							</div>
 							{/* .inputGroup */}
@@ -42,6 +128,8 @@ const Contact = ({ authKey }) => {
 									name="email"
 									id="email"
 									placeholder="hello@mycompany.com"
+									required
+									disabled={busy}
 								/>
 							</div>
 							{/* .inputGroup */}
@@ -52,6 +140,8 @@ const Contact = ({ authKey }) => {
 									name="organisation"
 									id="organisation"
 									placeholder="Organisation"
+									required
+									disabled={busy}
 								/>
 							</div>
 							{/* .inputGroup */}
@@ -63,10 +153,24 @@ const Contact = ({ authKey }) => {
 								name="message"
 								id="message"
 								placeholder="How can we help?"
+								required
+								disabled={busy}
 							></textarea>
 						</div>
 						{/* .inputGroup */}
-						<button>Submit</button>
+						<div className={style.bottom}>
+							<div
+								className={`${style.message} ${
+									submitMessage?.type === "error" ? style.error : ""
+								} ${submitMessage?.type === "success" ? style.success : ""}`}
+							>
+								{submitMessage ? submitMessage.message : "Message"}
+							</div>
+							<button disabled={busy}>
+								{busy ? <Loading height={10} /> : "Submit"}
+							</button>
+						</div>
+						{/* .bottom */}
 					</form>
 				</div>
 				{/* .contentWrapper */}
