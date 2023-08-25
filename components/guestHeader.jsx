bf94092a97f925a84ead3cffc2b3b81d30bc3beb
tomkochi <tomatkochi@gmail.com>
2023-08-26 01:53:10 +0530

    link to jobs added, but not shown

--- components/guestHeader.jsx
+++ components/guestHeader.jsx
@@ -2,18 +2,25 @@ import style from "./guestHeader.module.scss";
 import Link from "next/link";
 
 const GuestHeader = ({ children }) => {
-	return (
-		<div className={style.guestHeader}>
-			<div className={style.left}>{children}</div>
-			{/* .left */}
-			<div className={style.right}>
-				<a href="http://app.agrofocal.ai" className={style.login}>
-					Login
-				</a>
-			</div>
-			{/* .right */}
-		</div>
-	);
+  return (
+    <div className={style.guestHeader}>
+      <div className={style.left}>{children}</div>
+      {/* .left */}
+      <div className={style.right}>
+        <Link href="/jobs">
+          <a className={style.jobs}>Jobs</a>
+        </Link>
+        <a
+          href="http://app.agrofocal.ai"
+          className={style.login}
+          style={{ display: "none" }}
+        >
+          Login
+        </a>
+      </div>
+      {/* .right */}
+    </div>
+  );
 };
 
 export default GuestHeader;
