f3137c3fd51914b07804a547d3ffa0ff16d9386d
tomkochi <tomatkochi@gmail.com>
2023-08-28 08:54:52 +0530

    apply in separage page

--- pages/jobs/[slug].jsx
+++ pages/jobs/[slug].jsx
@@ -32,118 +32,21 @@ const JobPage = ({ jobData }) => {
             </div>
           </div>
           <div className={style.headingRight}>
-            <button onClick={() => setApply(true)}>Apply</button>
+            <Link
+              href={{
+                pathname: "/jobs/apply",
+                query: {
+                  rec: jobData.frontMatter.rec,
+                  position: jobData.frontMatter.position,
+                },
+              }}
+            >
+              <a>Apply</a>
+            </Link>
           </div>
         </div>
         <div className={style.html}>
           <ReactMarkdown>{jobData.markdownContent}</ReactMarkdown>
-        </div>
-      </div>
-      <div
-        className={style.applyOverlay}
-        style={apply ? null : { display: "none" }}
-      >
-        <div className={style.applyContainer}>
-          <form name="apply" data-netlify="true">
-            <input
-              type="text"
-              name="rec"
-              hidden
-              value={jobData.frontMatter.rec}
-            />
-            <input
-              type="text"
-              name="date"
-              hidden
-              value={jobData.frontMatter.date}
-            />
-            <input
-              type="text"
-              name="position"
-              hidden
-              value={jobData.frontMatter.position}
-            />
-            <div className={style.formHeader}>
-              <h2>Apply for - {jobData.frontMatter.position}</h2>
-              <button
-                type="button"
-                className={style.close}
-                onClick={() => setApply(false)}
-              >
-                <svg
-                  width="14"
-                  height="15"
-                  viewBox="0 0 14 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M13 1L1 13.6076"
-                    stroke="#343A40"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M1 1L13 13.6076"
-                    stroke="#343A40"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className={style.row}>
-              <div className={style.inputGroup}>
-                <label htmlFor="firstname">First name</label>
-                <input
-                  type="text"
-                  name="firstname"
-                  id="firstname"
-                  placeholder="First name"
-                  required
-                />
-              </div>
-              <div className={style.inputGroup}>
-                <label htmlFor="lastname">Last name</label>
-                <input
-                  type="text"
-                  name="lastname"
-                  id="lastname"
-                  placeholder="Last name"
-                  required
-                />
-              </div>
-            </div>
-            <div className={style.rowFull}>
-              <div className={style.inputGroup}>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder="hello@myid.com"
-                  required
-                />
-              </div>
-            </div>
-            <div className={style.rowFull}>
-              <div className={style.inputGroup}>
-                <label htmlFor="resume">Upload resume</label>
-                <input
-                  type="file"
-                  name="resume"
-                  id="resume"
-                  accept=".pdf,.doc,.docx"
-                  required
-                />
-              </div>
-            </div>
-            <button type="submit" className={style.submit}>
-              Submit
-            </button>
-          </form>
         </div>
       </div>
     </div>
